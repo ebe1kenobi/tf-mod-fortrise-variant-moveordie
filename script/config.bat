937f10f6ec8a79025a307368f94a1cf060c172a6
ebe1kenobi <ebe1.kenob.i@gmail.com>
2025-10-20 20:24:05 +0200

    change script and version required fortrise

--- script/config.bat
+++ script/config.bat
@@ -2,6 +2,7 @@ set REPO_DRIVE=D:
 set TOWERFALL_DRIVE=C:
 
 set DLL_MOD_FILE_NAME=TFModFortRiseVariantMoveOrDie.dll
+set PDB_MOD_FILE_NAME=TFModFortRiseVariantMoveOrDie.pdb
 set MODULE_DIR_NAME=tf-mod-fortrise-variant-moveordie
 
 set REPO_PATH=D:\__dev\code\tf-mod-fortrise-variant-moveordie\
@@ -13,6 +14,7 @@ set TOWERFALL_PATH="C:\Program Files (x86)\Steam\steamapps\common\TowerFall\"
 set TOWERFALL_MODS_PATH=%TOWERFALL_PATH%Mods\
 set TOWERFALL_THIS_MODULE_PATH=%TOWERFALL_MODS_PATH%%MODULE_DIR_NAME%\
 set TOWERFALL_EXE_PATH=%TOWERFALL_PATH%%EXE_TOWERFALL_NAME%
+set EXECUTABLE=%TOWERFALL_PATH%TowerFall.exe
 set PYRGOS_EXE_PATH="C:\Program Files\Pyrgos\Pyrgos.exe"
 
 
